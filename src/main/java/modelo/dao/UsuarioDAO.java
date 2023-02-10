1d51160611f5e9e2bb2111749f1c7ece34be5b3d
bryan <bryan.condoriv@gmail.com>
2023-02-10 15:14:30 -0300

    logic

--- src/main/java/modelo/dao/UsuarioDAO.java
+++ src/main/java/modelo/dao/UsuarioDAO.java
@@ -4,12 +4,75 @@
  */
 package modelo.dao;
 
-import mode.beans.Usuario;
 
+import java.util.ArrayList;
+import java.util.List;
+import mode.beans.Usuario;
 /**
  *
  * @author Bryan
  */
 public class UsuarioDAO {
-    private list<Usuario>;
-}
+
+    private List<Usuario> usuarios;
+
+    public UsuarioDAO() {
+        usuarios = new ArrayList<>();
+
+    }
+
+    public int buscar(String usuario) {
+        int n = -1;
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (usuarios.get(i).getUsuario().equals(usuario)) {
+                n = i;
+                break;
+            }
+        }
+        return n;
+    }
+
+    public boolean insertar(Usuario usuario){
+        if (buscar(usuario.getUsuario()) == -1) {
+            usuarios.add(usuario);
+            return true;
+        } else{
+            return false;
+        }   
+    }
+
+    public boolean modificar(Usuario usuario) {
+        if (buscar(usuario.getUsuario()) != -1) {
+            Usuario usuarioaux=obtener(usuario.getUsuario());
+            
+            usuarioaux.setContraseña(usuario.getContraseña());
+            usuarioaux.setNombres(usuario.getNombres());
+            usuarioaux.setApellidos(usuario.getApellidos());
+            usuarioaux.setCorreo(usuario.getCorreo());
+            return true;
+        } else{
+            return false;
+
+    }
+
+
+    public boolean eliminar(String usuario) {
+        if (buscar(usuario)!= -1) {
+            usuarios.remove(buscar(usuario));
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public Usuario obtener(String usuario) {
+        if (buscar(usuario)!=-1) {
+            return usuarios.get(buscar(usuario));
+        }else{
+            return null;
+        }
+    
+    }
+
+    }
+
--- src/main/java/modelo/logic/UsuarioLogic.java
+++ src/main/java/modelo/logic/UsuarioLogic.java
@@ -4,10 +4,42 @@
  */
 package modelo.logic;
 
-/**
- *
- * @author Bryan
- */
+import mode.beans.Usuario;
+import modelo.dao.UsuarioDAO;
+
 public class UsuarioLogic {
-    
+
+    private static UsuarioDAO usuariodao = new UsuarioDAO();
+
+    public static boolean autentificar(String usuario, String contraseña) {
+        if (obtener(usuario) != null) {
+            Usuario usuarioConsulta = obtener(usuario);
+        }
+        if (usuarioConsulta.getUsuario().equals(usuario) && usuarioConsulta.getContraseña().equals(contraseña)) {
+        }
+                return true;
+         else{
+                return false;
+            }
+       }else{
+         return false;
+       }
+    }
+
+    public static boolean insertar(Usuario usuario) {
+        return usuariodao.insertar(usuario);
+
+    }
+
+    public static boolean modificar(Usuario usuario) {
+        return usuariodao.modificar(usuario);
+    }
+
+    public static boolean eliminar(String usuario) {
+        return usuariodao.eliminar(usuario);
+    }
+
+    public static Usuario obtener(String usuario) {
+        return usuariodao.obtener(usuario);
+    }
 }
